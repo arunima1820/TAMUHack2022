7aced189d946894f0afb5fa2b44abf7c99872911
David Brooks <54827653+dmbeta@users.noreply.github.com>
2022-01-30 16:02:17 -0600

    properly loads from api, css grid

--- vinterest/src/Suggestions.tsx
+++ vinterest/src/Suggestions.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import robot from "./robot.png";
 import { useLocation } from 'react-router-dom';
 import { Line } from "@nivo/line";
 import envicon from "./envicon.png";
@@ -7,141 +8,115 @@ import animalicon from "./animalicon.png";
 import { ScrollMenu } from 'react-horizontal-scrolling-menu';
 import Header from "./Header";
 import backarrow from "./backarrow.png";
+import CircularProgress from '@mui/material/CircularProgress';
 export default function Suggestions(props: { data: any }) {
   const { state } = useLocation();
-  console.log(state)
   const [data, setData] = useState<any[]>([]);
-  let test=[{
-    ticker: "AAPL",
-    name: "Apple",
-    price: "178.28",
-    environmentalImpact: "A",
-    socialImpact: "A",
-    animalConservation: "B",
-    data: {
-      id: "AAPL",
-      data: [
-        { x: 1, y: 2 },
-        { x: 2, y: 2 },
-        { x: 3, y: 4 },
-        { x: 4, y: 6 },
-        { x: 5, y: 7 },
-        { x: 6, y: 3 },
-        { x: 7, y: 4 },
-        { x: 8, y: 4 },
-        { x: 9, y: 3 },
-        { x: 10, y: 5 },
-        { x: 11, y: 6 },
-        { x: 12, y: 8 },
-      ],
-    },
-  },
-  {
-    ticker: "MSFT",
-    name: "Microsoft",
-    price: "308.26",
-    environmentalImpact: "A",
-    socialImpact: "B",
-    animalConservation: "A",
-    data: {
-      id: "MSFT",
-      data: [
-        { x: 1, y: 2 },
-        { x: 2, y: 3 },
-        { x: 3, y: 4 },
-        { x: 4, y: 7 },
-        { x: 5, y: 8 },
-        { x: 6, y: 9 },
-        { x: 7, y: 10 },
-        { x: 8, y: 5 },
-        { x: 9, y: 3 },
-        { x: 10, y: 2 },
-        { x: 11, y: 1 },
-        { x: 12, y: 5 },
-      ],
-    },
-  },
-  {
-    ticker: "CRNC",
-    name: "Cerence Inc",
-    price: "59.73",
-    environmentalImpact: "A",
-    socialImpact: "B",
-    animalConservation: "C",
-    data: {
-      id: "CRNC",
-      data: [
-        { x: 1, y: 5 },
-        { x: 2, y: 6 },
-        { x: 3, y: 7 },
-        { x: 4, y: 3 },
-        { x: 5, y: 2 },
-        { x: 6, y: 2 },
-        { x: 7, y: 1 },
-        { x: 8, y: 2 },
-        { x: 9, y: 3 },
-        { x: 10, y: 6 },
-        { x: 11, y: 7 },
-        { x: 12, y: 9 },
-      ],
-    },
-  },
-];
   useEffect(() => {
-    //axios.post(....)
-    setData(test);
-  },[]);
-  
+    if (!state) {
+      return;
+    }
+    // axios.post(state)
+    fetch("http://localhost:8080/recommendations", {
+      method: "POST",
+      mode: "cors",
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(state)
+    }).then(data => {
+      return data.json()
+    }).then(res => { console.log(res.data); setData(JSON.parse(JSON.stringify(res.data))); });
+  }, []);
 
+  if (data.length === 0 && state) {
+    return (
+      <>
+        <Header page={3} />
+        <div
+          style={{
+            flex: 1,
+            fontSize: "24px",
+            textAlign: "center",
+            color: "black",
+            marginTop: "30px",
+          }}
+        >
+          <div
+            style={{
+              padding: "20px",
+              borderRadius: "10px",
+              alignItems: "center",
+            }}
+          >
+            <img
+              src={robot}
+              alt="robot"
+              style={{
+                height: "64px",
+                marginRight: "10px",
+              }}
+            ></img>
+            <div>
+              I'm loading up your portfolio data now, this may take a second<br></br>
+            </div>
+          </div>
+          <br />
+          <br />
+          <CircularProgress />
+        </div>
 
-  return (
-    <>
-      <Header page={3} />
-      <div
-        style={{
-          flex: 1,
-          fontWeight: "bold",
-          fontSize: "24px",
-          textAlign: "center",
-          color: "black",
-          marginTop: "30px",
-        }}
-      >
-        Our Suggestions For You
-      </div>
-      <button
-        style={{
-          color: "black",
-          backgroundColor: "#F5F5F5",
-          borderRadius: "30px",
-          borderWidth: "0px",
-          fontSize: "16px",
-          fontWeight: "bold",
-          padding: "20px",
-          alignItems: "left",
-          justifyContent: "left",
-          alignContent: "center",
-          flexDirection: "row",
-          display: "flex",
-          flex: 1,
-          marginLeft: "50px",
-          marginRight: "50px",
-        }}
-      >
-        <img
-          src={backarrow}
-          alt="backarrow"
-          style={{ height: 18, marginRight: "10px" }}
-        />
-        <div>Edit Preferences</div>
-      </button>
-      <ScrollMenu>
-        {data.map((item: any) => {
-          return <Card data={item}></Card>;
-        })}
-      </ScrollMenu>
-    </>
-  );
+      </>
+    )
+  }
+  else {
+    return (
+      <>
+        <Header page={3} />
+        <div
+          style={{
+            flex: 1,
+            fontWeight: "bold",
+            fontSize: "24px",
+            textAlign: "center",
+            color: "black",
+            marginTop: "30px",
+          }}
+        >
+          Our Suggestions For You
+        </div>
+        <button
+          style={{
+            color: "black",
+            backgroundColor: "#F5F5F5",
+            borderRadius: "30px",
+            borderWidth: "0px",
+            fontSize: "16px",
+            fontWeight: "bold",
+            padding: "20px",
+            alignItems: "left",
+            justifyContent: "left",
+            alignContent: "center",
+            flexDirection: "row",
+            display: "flex",
+            flex: 1,
+            marginLeft: "50px",
+            marginRight: "50px",
+          }}
+        >
+          <img
+            src={backarrow}
+            alt="backarrow"
+            style={{ height: 18, marginRight: "10px" }}
+          />
+          <div>Edit Preferences</div>
+        </button>
+        <div style={{ display: "flex", flexWrap: "wrap", padding: "2rem", justifyContent: "center" }}>
+          {data.map((item: any) => {
+            return <Card data={item}></Card>;
+          })}
+        </div>
+      </>
+    );
+  }
 }
 
 function Card(props: { data: any }) {
@@ -156,6 +131,7 @@ function Card(props: { data: any }) {
         marginBottom: "30px",
         flex: 1,
         width: "530px",
+        maxWidth: "530px",
         borderRadius: "30px",
         padding: "40px",
       }}>
@@ -219,7 +195,7 @@ function Card(props: { data: any }) {
           >$ {props.data.price}</div>
         </div>
       </div>
-      
+
       <Line
         data={[props.data.data]}
         margin={{ top: 50, right: 0, bottom: 50, left: 0 }}
@@ -234,7 +210,7 @@ function Card(props: { data: any }) {
           stacked: true,
           reverse: false,
         }}
-        colors= {["#B75CFF"]}
+        colors={["#B75CFF"]}
         lineWidth={3}
         yFormat=" >-.2f"
         axisTop={null}
@@ -258,15 +234,15 @@ function Card(props: { data: any }) {
         borderTop: "1px solid #EFEFEF",
         paddingTop: "20px",
         paddingBottom: "20px",
-        alignItems:"center",
+        alignItems: "center",
       }}>
-        <img src={envicon} alt={"envicon"} style={{width:15, height:15}}/>
+        <img src={envicon} alt={"envicon"} style={{ width: 15, height: 15 }} />
         <div style={{
           fontSize: "18px",
           fontWeight: "bold",
           marginLeft: "20px",
         }}>Environmental Rating</div>
-        <div style={{flex:1}}></div>
+        <div style={{ flex: 1 }}></div>
         <div style={{
           fontSize: "24px",
           fontWeight: "bold",
@@ -281,15 +257,15 @@ function Card(props: { data: any }) {
         borderTop: "1px solid #EFEFEF",
         paddingTop: "20px",
         paddingBottom: "20px",
-        alignItems:"center",
+        alignItems: "center",
       }}>
-        <img src={socialicon} alt={"envicon"} style={{width:15, height:15}}/>
+        <img src={socialicon} alt={"envicon"} style={{ width: 15, height: 15 }} />
         <div style={{
           fontSize: "18px",
           fontWeight: "bold",
           marginLeft: "20px",
         }}>Social Impact Rating</div>
-        <div style={{flex:1}}></div>
+        <div style={{ flex: 1 }}></div>
         <div style={{
           fontSize: "24px",
           fontWeight: "bold",
@@ -304,15 +280,15 @@ function Card(props: { data: any }) {
         borderTop: "1px solid #EFEFEF",
         paddingTop: "20px",
         paddingBottom: "20px",
-        alignItems:"center",
+        alignItems: "center",
       }}>
-        <img src={animalicon} alt={"envicon"} style={{width:15, height:15}}/>
+        <img src={animalicon} alt={"envicon"} style={{ width: 15, height: 15 }} />
         <div style={{
           fontSize: "18px",
           fontWeight: "bold",
           marginLeft: "20px",
         }}>Animal Conservation</div>
-        <div style={{flex:1}}></div>
+        <div style={{ flex: 1 }}></div>
         <div style={{
           fontSize: "24px",
           fontWeight: "bold",
@@ -323,8 +299,8 @@ function Card(props: { data: any }) {
   );
 }
 
-function getColor(rating:string){
-  switch(rating){
+function getColor(rating: string) {
+  switch (rating) {
     case "A":
       return "#00AD26";
     case "B":
