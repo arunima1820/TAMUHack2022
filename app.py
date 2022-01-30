1615057fb8d2f3363a3ed284ede91eaef77f7368
David B <54827653+dmbeta@users.noreply.github.com>
2022-01-29 19:33:40 -0600

    init

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 from flask import request
 
 app = Flask(__name__)
@@ -10,6 +10,8 @@ app = Flask(__name__)
 #   "socialrating": number,
 #   "issuescareabout": [list of strings],
 # }
+
+
 @app.route("/recommendations", methods=["POST"])
 def getStockRecommendations():
     envRating = int(request.form['envrating'])
@@ -18,8 +20,25 @@ def getStockRecommendations():
 
     return stockRecommendations(envRating, socialRating, issuesCareAbout)
 
+
 def stockRecommendations():
     pass
+
+
+@app.route("/start")
+def start():
+    return render_template("getstarted.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
